d190d2a3e867d278119ae5874d13962a4a77d9eb
fraol lemma <fraollemma0506@gmail.com>
2025-04-03 23:28:48 +0300

    fraol4

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-release: python manage.py migrate
+release: python manage.py check --deploy && python manage.py migrate
 web: gunicorn puddle.wsgi --workers=$WEB_CONCURRENCY --timeout 120 --bind 0.0.0.0:$PORT
--- puddle/settings.py
+++ puddle/settings.py
@@ -63,11 +63,11 @@ INSTALLED_APPS = [
     'item',
 ]
 
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -168,14 +168,12 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
-STATIC_ROOT = 'staticfiles'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 # MEDIA_URL = '/media/'
 # MEDIA_ROOT = BASE_DIR / 'media'
