dbd22a1fb08b895720266b22a656433e160ec4d4
fraol lemma <fraollemma0506@gmail.com>
2025-04-08 14:44:25 +0300

    cloudiy

--- puddle/settings.py
+++ puddle/settings.py
@@ -25,11 +25,12 @@ cloudinary.config(
     url=os.getenv('CLOUDINARY_URL')
 )
 
-CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.getenv('CLOUD_NAME'),
-    'API_KEY': os.getenv('CLOUD_API_KEY'),
-    'API_SECRET': os.getenv('CLOUD_API_SECRET')
-}
+cloudinary.config(
+    cloud_name="doixo5oiw",
+    api_key="435759228322341",
+    api_secret="H3_ZVEXWGcyuE28IfKWUYsTo5sY"
+)
+
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
